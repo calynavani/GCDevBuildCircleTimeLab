111ddaa469b2b4842d89c16e1fdfab1994c08e40
Calyn Greene <calyngreene@rocketmortgage.com>
2021-10-19 17:08:51 -0400

    inital commit for the Circle Time Lab

---
+++ GCDevBuildCircleTimeLab/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCDevBuildCircleTimeLab
+{
+    class Circle //This class will store data about the circle
+    {
+
+      
+
+        //Properties
+        public double radius { get; set; }
+
+
+        public Circle(double radius)//Constructor
+        {
+            this.radius = radius;
+
+        }
+
+        public double CalculateCircuference(double radius)
+        {
+            double circum = 2 * Math.PI * radius;
+            
+            Console.WriteLine($"Cirumference: {circum}");
+           // this.circum = circum;
+            return circum;
+        }
+
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * (radius * radius);
+            Console.WriteLine($"Area: {area}");
+           // this.area = area;
+            return area;
+        }
+        public override string ToString()
+        {
+            
+            string output = $"Radius: {radius} \n";
+            output += $"Circumference: {CalculateCircuference(radius)} \n";
+            output += $"Area: {CalculateArea(radius)} \n";
+
+            return output;
+        }
+
+    }
+}
---
+++ GCDevBuildCircleTimeLab/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GCDevBuildCircleTimeLab
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester");
+            bool goOn = true;
+            
+            while (goOn == true)
+            {
+
+
+                Console.WriteLine();
+                double userRadius = GetRadius();
+
+                //make a list for the circles to go into
+                List<Circle> ListOfCircles = new List<Circle>();
+
+                //adds a new circle to the list each radius thats entered
+                ListOfCircles.Add(new Circle(userRadius));
+
+                //create a circle object
+                Circle c = new Circle(userRadius);
+
+                //Display circumerence
+                c.CalculateCircuference(userRadius);
+
+                //Display area
+                c.CalculateArea(userRadius);
+
+                Console.WriteLine(ListOfCircles.Count);
+
+                //call the continue method
+                if (Continue() == false)
+                {
+                    foreach (var circle in ListOfCircles)//print the list of circles
+                    {
+                        Console.WriteLine(circle);
+                  
+                        break;
+                    }
+
+                }
+
+
+            }
+        }
+            public static double GetRadius()
+            {
+                Console.WriteLine();
+                Console.Write("Enter Radius: ");
+                double radius = double.Parse(Console.ReadLine());
+
+
+               
+
+                return radius;
+            }
+            public static bool Continue()
+            {
+                Console.Write("Would you like to continue? y/n: ");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "y")
+                {
+                    return true;
+                }
+                else if (answer == "n")
+                {
+                    Console.WriteLine("Goodbye!");
+                    return false;
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("I'm sorry I didn't understand");
+                    Console.WriteLine("Let's try again.");
+
+
+                    return Continue();
+
+                }
+            }
+
+        }
+    }
+
